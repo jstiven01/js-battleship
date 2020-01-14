3ef268c955b92fe0ebb48299a60b8efa7755206d
vieiramanda11 <amanda_vieira@outlook.com>
2020-01-14 18:13:13 -0300

    writing TDD to not allowed same ship in horizontal positions

--- src/js/gameBoard.js
+++ src/js/gameBoard.js
@@ -18,12 +18,24 @@ const gameBoard = () => {
 
     const validatePosition = (ship, position, orientation) => {
         for(let i = 0; i < shipsPositions.length; i+=1){
+            const initialRow = shipsPositions[i].position.row;
+            const initialCol = shipsPositions[i].position.column;
+            const finalRow = shipsPositions[i].position.row + ship.length - 1;
+            const finalCol = shipsPositions[i].position.column + ship.length - 1;
 
             console.log((shipsPositions[i].row === position.row), (shipsPositions.column === position.column))
             //console.log()
-            if(shipsPositions[i].orientation === 'V' && orientation ==='V' 
-            && shipsPositions[i].position.row === position.row 
-            && shipsPositions[i].position.column === position.column ) {
+            if((shipsPositions[i].orientation === 'V' && orientation ==='V'
+            || shipsPositions[i].orientation === 'H' && orientation ==='H')  
+            && initialRow === position.row 
+            && initialCol === position.column ) {
+                return false;
+            }
+
+            if(shipsPositions[i].orientation === 'H' && orientation === 'V'
+            && (position.row <= initialRow
+            && position.row + ship.length - 1 >= initialRow)
+            && (initialCol <= position.column && position.column <= finalCol))  {
                 return false;
             }
 
--- test/gameBoard.test.js
+++ test/gameBoard.test.js
@@ -23,7 +23,28 @@ test.only('placing ship in a taken position both of them vertical', () => {
 
 });
 
-test.todo('placing ship in a taken position both of them Horizontal');
-test.todo('placing ship in same occupied position oldHorizontal NewVertical');
+test('placing ship in a taken position both of them Horizontal', () => {
+    const ship1 = Ship(2);
+    const gameBoardHuman = gameBoard();
+    const initialPosition1 = { row: 1, column: 5};
+    const orientation1 = 'H';
+    const ship2 = Ship(4);
+    gameBoardHuman.placeShip(ship1, initialPosition1, orientation1);
+    const isTaken = gameBoardHuman.placeShip(ship2, initialPosition1, orientation1);
+    expect(isTaken).toBe(false);
+});
+
+test.only('placing ship in same occupied position oldHorizontal NewVertical', () => {
+    const ship1 = Ship(2);
+    const gameBoardHuman = gameBoard();
+    const initialPosition1 = { row: 4, column: 4};
+    const orientation1 = 'H';
+    const ship2 = Ship(4);
+    const initialPosition2 = { row: 1, column: 7};
+    const orientation2 = 'V';
+    gameBoardHuman.placeShip(ship1, initialPosition1, orientation1);
+    const isTaken = gameBoardHuman.placeShip(ship2, initialPosition2, orientation2);
+    expect(isTaken).toBe(false);
+});
 test.todo('placing ship in available position oldHorizontal newVertical');
 
