e3e993765b47285f108ddda31814a8cd3e5d797c
vieiramanda11 <amanda_vieira@outlook.com>
2020-01-14 13:20:22 -0300

    writing TDD and create ship factory

---
+++ src/js/ship.js
@@ -0,0 +1,19 @@
+const Ship = (length) => {
+  const ship = Array(length).fill(false);
+  
+  const hit = (position) => {
+    if (ship[position] === false) {
+      ship[position] = true;
+    }
+  }
+
+  const isSunk = () => {
+    return ship.every((position) => position === true);
+  }
+
+  const getType = () => `${length}`;
+
+  return { hit, isSunk, getType }
+}
+
+export default Ship;
