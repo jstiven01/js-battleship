ac6abfecdc8caba8bb922a606166cddf839e8c95
vieiramanda11 <amanda_vieira@outlook.com>
2020-01-15 18:49:41 -0300

    creating play human event

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import GameBoard from '../src/js/gameBoard';
 import Player from '../src/js/player';
 import Ship from '../src/js/ship';
 import UI from '../src/js/ui'
+import gameBoard from '../src/js/gameBoard';
 
 const arrayShipsH = [Ship(5), Ship(4), Ship(3), Ship(3), Ship(2)];
 const arrayShipsC = [Ship(5), Ship(4), Ship(3), Ship(3), Ship(2)];
@@ -19,3 +20,22 @@ playerComputer.setShips(gameBoardC);
 
 UI.renderInitialBoards(playerHuman, gameBoardH);
 UI.renderInitialBoards(playerComputer, gameBoardC);
+
+const buttons = document.querySelectorAll('.button-position-player2');
+
+const playHuman = (event) => {
+  if (!gameBoardC.isOver() && playerHuman.getTurn() && !gameBoardH.isOver()) {
+    const row = parseInt(event.target.dataset.row, 10);
+    const column = parseInt(event.target.dataset.column, 10);
+    playerHuman.attackRival(gameBoardC, {row, column});
+    if (gameBoardC.getHit()) {
+      playerHuman.setTurn(true);
+      playerComputer.setTurn(false);
+    } else {
+      playerHuman.setTurn(false);
+      playerComputer.setTurn(true);
+    }
+  }
+}
+
+buttons.forEach(button => button.addEventListener('click', playHuman));
