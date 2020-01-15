e056228c63c2d8afed31daafbfab5d74cd11a1a6
vieiramanda11 <amanda_vieira@outlook.com>
2020-01-15 18:12:10 -0300

    creating intial settings to start game and UI module

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './scss/style.scss';
+import GameBoard from '../src/js/gameBoard';
+import Player from '../src/js/player';
+import Ship from '../src/js/ship';
+
+const arrayShipsH = [Ship(5), Ship(4), Ship(3), Ship(3), Ship(2)];
+const arrayShipsC = [Ship(5), Ship(4), Ship(3), Ship(3), Ship(2)];
+const gameBoardH = GameBoard();
+const gameBoardC = GameBoard();
+const playerComputer = Player('Player Computer', 'C', arrayShipsC);
+const playerHuman = Player('Player Human', 'H', arrayShipsH);
+
+playerHuman.setTurn(true);
+playerComputer.setTurn(false);
+
+playerHuman.setShips(gameBoardH);
+playerComputer.setShips(gameBoardC);
+
---
+++ src/js/ui.js
@@ -0,0 +1,12 @@
+const UI = (() => {
+
+  const renderInitialBoards = (player, gameBoard) => {
+    for (let i = 0; i < gameBoard.getBoard().lenght; i +=1) {
+      for (let j = 0; j < gameBoard.getBoard().lenght; j +=1) {
+        
+      }
+    }
+  }
+})();
+
+export default UI;
